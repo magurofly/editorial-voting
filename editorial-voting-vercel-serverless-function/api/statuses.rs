3a21a35fe45c8a1be7f1daea1a84a80b245ebed6
magurofly <54321436+magurofly@users.noreply.github.com>
2025-02-24 20:33:23 +0900

    make /statuses faster

--- editorial-voting-vercel-serverless-function/api/statuses.rs
+++ editorial-voting-vercel-serverless-function/api/statuses.rs
@@ -17,11 +17,22 @@ struct Res {
     results: Option<Vec<SingleRes>>,
 }
 
-#[derive(serde::Serialize, Default, Debug)]
+#[derive(serde::Serialize, Clone, Default, Debug)]
 struct SingleRes {
     score: i64,
     scores_by_rating: HashMap<String, i64>,
     current_vote: Option<&'static str>,
+}
+
+#[derive(serde::Serialize, Default, Debug)]
+struct EditorialUrlQuery {
+    index: i32,
+    query: String,
+}
+
+#[derive(serde::Serialize, Default, Debug)]
+struct IdQuery {
+    id: i32,
 }
 
 #[tokio::main]
@@ -38,7 +49,8 @@ pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
     if req.method() == "OPTIONS" {
         return Ok(Response::builder()
             .status(StatusCode::NO_CONTENT)
-            .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Origin", "atcoder.jp")
+            .header("Access-Control-Allow-Credentials", "true")
             .header("Access-Control-Allow-Methods", "*")
             .header("Access-Control-Allow-Headers", "*")
             .header("Access-Control-Max-Age", "86400")
@@ -51,7 +63,8 @@ pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
-        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Origin", "atcoder.jp")
+        .header("Access-Control-Allow-Credentials", "true")
         .header("Access-Control-Allow-Headers", "*")
         .body(Body::Text(serde_json::to_string(&res)?))?)
 }
@@ -69,45 +82,61 @@ async fn proc(req: Request) -> Result<Res, Box<dyn std::error::Error>> {
         if let Some(token) = req.token.as_ref() {
             let user_token = atcoder_api::parse_token(token)?;
 
-            let results = req.editorials.iter().map(|editorial| -> Result<SingleRes, Box<dyn std::error::Error>> {
-                // get editorial_id
-                let Some(editorial_url) = atcoder_api::canonicalize_editorial_url(&editorial) else {
-                    return Err("invalid editorial URL".into());
-                };
-                let rows = client.query("SELECT id FROM editorials WHERE editorial = $1", &[&editorial_url])?;
-                if rows.is_empty() {
-                    return Ok(SingleRes {
-                        score: 0,
-                        current_vote: Some("none"),
-                        .. Default::default()
-                    });
-                }
-                let editorial_id = rows[0].get::<_, i32>(0);
-
-                // get score
-                let mut score = 0;
-                let mut scores_by_rating = HashMap::new();
-                let rows = client.query("SELECT rating_level, score FROM vote_temp WHERE editorial_id = $1", &[&editorial_id])?;
+            // get editorial_ids
+            let (editorial_ids, editorial_id_map) = {
+                let query_records = req.editorials.iter().enumerate().map(|(index, query)| EditorialUrlQuery { index: index as i32, query: query.to_string() } ).collect::<Vec<_>>();
+                let json = serde_json::to_value(&query_records)?;
+                let rows = client.query("SELECT id, index FROM editorials, JSON_TO_RECORDSET($1) AS queries(index INTEGER, query TEXT) WHERE editorial = query", &[&json])?;
+                // return (id, index)
+                let mut editorial_ids = vec![];
+                let mut editorial_id_map = HashMap::new();
                 for row in rows {
-                    let rating_level = row.get::<_, i16>(0) as usize;
-                    let score_by_rating_level = row.get::<_, i32>(1) as i64;
-                    score += score_by_rating_level;
-                    scores_by_rating.insert(format!("{}-{}", rating_level * 100, rating_level * 100 + 99), score_by_rating_level);
+                    let id = row.get::<_, i32>(0);
+                    let index = row.get::<_, i32>(1) as usize;
+                    editorial_ids.push(id);
+                    editorial_id_map.insert(id, index);
                 }
-                
-                // get current vote
-                let current_vote = Some(match client.query_opt("SELECT score FROM votes WHERE user_id = $1 AND editorial_id = $2", &[&user_token.user_id, &editorial_id])?.map(|row| row.get::<_, i16>(0) ) {
-                    Some(1) => "up",
-                    Some(-1) => "down",
-                    _ => "none"
-                });
+                (editorial_ids, editorial_id_map)
+            };
 
-                Ok(SingleRes {
-                    score,
-                    scores_by_rating,
-                    current_vote,
-                })
-            }).collect::<Result<Vec<SingleRes>, _>>()?;
+            let mut results = vec![SingleRes {
+                current_vote: Some("none"),
+                ..Default::default()
+            }; req.editorials.len()];
+
+            let editorial_ids_json = serde_json::to_value(&editorial_ids.iter().map(|&id| IdQuery { id } ).collect::<Vec<_>>())?;
+
+            // get scores
+            {
+                let rows = client.query("SELECT id, rating_level, score FROM vote_temp, JSON_TO_RECORDSET($1) AS queries(id INTEGER) WHERE editorial_id = queries.id", &[&editorial_ids_json])?;
+                for row in rows {
+                    let id = row.get::<_, i32>(0);
+                    let rating_level = row.get::<_, i16>(1) as usize;
+                    let score_by_rating_level = row.get::<_, i32>(2) as i64;
+                    let index = editorial_id_map[&id];
+                    results[index].score += score_by_rating_level;
+                    results[index].scores_by_rating.insert(format!("{}-{}", rating_level * 100, rating_level * 100 + 99), score_by_rating_level);
+                }
+            }
+
+            // get current votes
+            {
+                let rows = client.query("SELECT id, score FROM votes, JSON_TO_RECORDSET($1) AS queries(id INTEGER) WHERE user_id = $2 AND editorial_id = queries.id", &[&editorial_ids_json, &user_token.user_id])?;
+                for row in rows {
+                    let id = row.get::<_, i32>(0);
+                    let score = row.get::<_, i16>(1);
+                    let index = editorial_id_map[&id];
+                    match score {
+                        1 => {
+                            results[index].current_vote = Some("up");
+                        }
+                        -1 => {
+                            results[index].current_vote = Some("down");
+                        }
+                        _ => {}
+                    }
+                }
+            }
 
             Ok(Res {
                 status: "success",
@@ -115,24 +144,19 @@ async fn proc(req: Request) -> Result<Res, Box<dyn std::error::Error>> {
                 .. Default::default()
             })
         } else {
-            let results = req.editorials.iter().map(|editorial| -> Result<SingleRes, Box<dyn std::error::Error>> {
-                // get score
-                let rows = client.query("SELECT rating_level, score FROM vote_temp, editorials WHERE editorial_id = editorials.id AND editorials.editorial = $1", &[editorial])?;
-                let mut score = 0;
-                let mut scores_by_rating = HashMap::new();
-                for row in rows {
-                    let rating_level = row.get::<_, i16>(0) as usize;
-                    let score_by_rating_level = row.get::<_, i32>(1) as i64;
-                    score += score_by_rating_level;
-                    scores_by_rating.insert(format!("{}-{}", rating_level * 100, rating_level * 100 + 99), score_by_rating_level);
-                }
+            let mut results = vec![SingleRes::default(); req.editorials.len()];
 
-                Ok(SingleRes {
-                    score,
-                    scores_by_rating,
-                    .. Default::default()
-                })
-            }).collect::<Result<Vec<SingleRes>, _>>()?;
+            // get scores
+            let query_records = req.editorials.iter().enumerate().map(|(index, query)| EditorialUrlQuery { index: index as i32, query: query.to_string() } ).collect::<Vec<_>>();
+            let json = serde_json::to_value(&query_records)?;
+            let rows = client.query("SELECT index, rating_level, score FROM editorials, vote_temp, JSON_TO_RECORDSET($1) AS queries(index INTEGER, query TEXT) WHERE editorial_id = id AND editorial = query", &[&json])?;
+            for row in rows {
+                let index = row.get::<_, i32>(0) as usize;
+                let rating_level = row.get::<_, i16>(1) as usize;
+                let score_by_rating_level = row.get::<_, i32>(2) as i64;
+                results[index].score += score_by_rating_level;
+                results[index].scores_by_rating.insert(format!("{}-{}", rating_level * 100, rating_level * 100 + 99), score_by_rating_level);
+            }
 
             Ok(Res {
                 status: "success",
